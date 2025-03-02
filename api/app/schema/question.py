fac7d595a9cb0b776171dc7d0304e5240434fa74
Delemangi <milev.stefan@gmail.com>
2025-03-03 00:15:50 +0100

    Setup validation

---
+++ api/app/schema/question.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class QuestionSchema(BaseModel):
+    id: UUID
+    name: str
+    content: str
+    user_id: str
+    links: dict[str, str] | None
+    created_at: datetime
+    updated_at: datetime
+
+
+class CreateQuestionSchema(BaseModel):
+    name: str
+    content: str
+    user_id: str
+    links: dict[str, str] | None = Field(default=None)
+
+
+class UpdateQuestionSchema(BaseModel):
+    name: str | None = Field(default=None)
+    content: str | None = Field(default=None)
+    user_id: str | None = Field(default=None)
+    links: dict[str, str] | None = Field(default=None)
