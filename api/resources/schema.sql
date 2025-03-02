0876a5cc829ad9c838149d1f07484e43d5867cef
Delemangi <milev.stefan@gmail.com>
2025-03-03 00:00:37 +0100

    Create database schema

---
+++ api/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS question (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    name TEXT NOT NULL UNIQUE,
+    content TEXT NOT NULL,
+    user_id TEXT,
+    links JSONB,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS question_name_idx ON question (name);
+
+CREATE TABLE IF NOT EXISTS link (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    name TEXT NOT NULL UNIQUE,
+    url TEXT NOT NULL,
+    description TEXT,
+    user_id TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS link_name_idx ON link (name);
