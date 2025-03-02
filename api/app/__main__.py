f13e0ea4170f1891bd5d1e8c083230e8edc8fb08
Delemangi <milev.stefan@gmail.com>
2025-03-03 00:16:17 +0100

    Setup FastAPI server

--- api/app/__main__.py
+++ api/app/__main__.py
@@ -1,14 +1,34 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
+from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from app.api.questions import router as questions_router
+from app.constants import strings
+from app.data.connection import Database
+
+schema_path = Path.cwd() / "resources" / "schema.sql"
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncGenerator:
+    db = Database()
+
+    await run_migration(db)
+
     yield
+
+    await db.disconnect()
+
+
+async def run_migration(database: Database) -> None:
+    with Path.open(schema_path) as f:
+        sql = f.read()
+
+    await database.execute(sql)
 
 
 def make_app() -> FastAPI:
@@ -23,16 +43,18 @@ def make_app() -> FastAPI:
         expose_headers=["*"],
     )
 
-    @app.get("/")
+    app.include_router(questions_router, prefix="/questions")
+
+    @app.get("/", tags=["Root"])
     async def root() -> str:
-        return "The API is running!"
+        return strings.alive_response
 
     return app
 
 
 def main() -> None:
     app = make_app()
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=8880, host="0.0.0.0")
 
 
 if __name__ == "__main__":
