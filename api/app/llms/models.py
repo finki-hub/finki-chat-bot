4e3bcad15124e502854357136695305d95a2e6eb
Delemangi <milev.stefan@gmail.com>
2025-06-04 22:15:30 +0200

    Support more models

--- api/app/llms/models.py
+++ api/app/llms/models.py
@@ -9,6 +9,9 @@ class Model(Enum):
     LLAMA_3_3_70B = "llama3.3:70b"
     BGE_M3 = "bge-m3:latest"
     MISTRAL = "mistral:latest"
+    DEEPSEEK_R1_70B = "deepseek-r1:70b"
+    QWEN_2_5_72B = "qwen2.5:72b"
+    DOMESTIC_YAK_8B_INSTRUCT_GGUF = "hf.co/LVSTCK/domestic-yak-8B-instruct-GGUF:Q8_0"
 
 
 MODEL_EMBEDDINGS_COLUMNS: dict[Model, str] = {
